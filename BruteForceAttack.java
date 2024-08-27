5dcd9ad288dd6f2b2014256c100fe6798e00adc3
shivabalan1712 <shivabalan2002@gmail.com>
2024-08-27 17:45:08 +0530

    Initialized repo

--- BruteForceAttack.java
+++ BruteForceAttack.java
@@ -1,34 +1,37 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class BruteForceAttack{
-    public static StringBuffer decrypt(String text, int s){
-        StringBuffer result = new StringBuffer();
-        for(int i=0;i<text.length();i++){
-            if(Character.isUpperCase(text.charAt(i))){
-                char ch = (char)(((((int)text.charAt(i) - 65) - s + 26) % 26) + 65);
-                result.append(ch);
-            }
-            else{
-                char ch = (char)(((((int)text.charAt(i) - 97) - s + 26) % 26) + 97);
-                result.append(ch);
-            }
-        }
-        return result;
-    }
-
-    public static void BruteForceAttack(String encryptedtext){
-        for(int s=1;s<26;s++){
-            StringBuffer decryptedtext = decrypt(encryptedtext,s);
-            System.out.println("Shift: "+s+" -> "+decryptedtext);
-        }
-    }
-
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Encrypted text: ");
-        String encryptedtext = sc.nextLine();
-        BruteForceAttack(encryptedtext);
-        sc.close();
+public class BruteForceAttack {
     
+    public static void bruteForceAttack(String encryptedText) {
+        StringBuilder result = new StringBuilder();
+        
+        for (int s = 1; s < 26; s++) {
+            result.setLength(0); // Reset the StringBuilder for the next shift
+            
+            for (int i = 0; i < encryptedText.length(); i++) {
+                char ch = encryptedText.charAt(i);
+                
+                if (Character.isUpperCase(ch)) {
+                    ch = (char) ((ch - 65 - s + 26) % 26 + 65);
+                } else if (Character.isLowerCase(ch)) {
+                    ch = (char) ((ch - 97 - s + 26) % 26 + 97);
+                }
+                
+                result.append(ch);
+            }
+            
+            System.out.println("Shift: " + s + " -> " + result.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter an Encrypted text: ");
+        String encryptedText = sc.nextLine();
+        
+        bruteForceAttack(encryptedText);
+        
+        sc.close();
     }
 }
