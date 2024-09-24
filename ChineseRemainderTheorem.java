2dc73a4514ebf9c38347455a6324a9bb6090dc19
shivabalan1712 <shivabalan2002@gmail.com>
2024-09-24 18:21:14 +0530

    Scripts added

--- ChineseRemainderTheorem.java
+++ ChineseRemainderTheorem.java
@@ -2,27 +2,23 @@ import java.util.Scanner;
 
 public class ChineseRemainderTheorem {
 
-    // Function to perform Extended Euclidean Algorithm
     public static int gcdExtended(int a, int b, int[] x, int[] y) {
-        // Base case
         if (a == 0) {
             x[0] = 0;
             y[0] = 1;
             return b;
         }
 
-        int[] x1 = new int[1]; // To store results of recursive call
-        int[] y1 = new int[1]; // To store results of recursive call
+        int[] x1 = new int[1]; 
+        int[] y1 = new int[1]; 
         int gcd = gcdExtended(b % a, a, x1, y1);
 
-        // Update x and y using results of recursive call
         x[0] = y1[0] - (b / a) * x1[0];
         y[0] = x1[0];
 
         return gcd;
     }
 
-    // Function to find the modular inverse of a under modulo m
     public static int modInverse(int a, int m) {
         int[] x = new int[1];
         int[] y = new int[1];
@@ -30,12 +26,10 @@ public class ChineseRemainderTheorem {
         if (g != 1) {
             throw new ArithmeticException("Inverse doesn't exist");
         } else {
-            // m is added to handle negative x
             return (x[0] % m + m) % m;
         }
     }
 
-    // Function to solve the system of congruences
     public static int chineseRemainderTheorem(int[] a, int[] m) {
         int prod = 1;
         for (int i : m) {
@@ -55,8 +49,8 @@ public class ChineseRemainderTheorem {
 
         System.out.print("Enter the number of equations: ");
         int n = scanner.nextInt();
-        int[] a = new int[n]; // Remainders
-        int[] m = new int[n]; // Moduli
+        int[] a = new int[n]; 
+        int[] m = new int[n]; 
 
         for (int i = 0; i < n; i++) {
             System.out.print("Enter remainder a[" + i + "]: ");
